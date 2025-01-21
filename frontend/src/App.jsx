444b3f340fcb598426b3cb199a0064a361934d24
Gaidysheff <gaidysheff@mail.ru>
2025-01-21 12:57:03 +0500

    test-1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+      <h1 className="text-7xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
